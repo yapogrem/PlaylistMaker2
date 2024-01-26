33c0dda33921508b91cfd20c7beb5e1ace0ea9e4
pogre <0961@list.ru>
2024-01-26 13:18:04 +0300

    complite 1p

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayFind = findViewById<Button>(R.id.find)
+        displayFind.setOnClickListener {
+            val displayIntent = Intent(this, Find::class.java)
+            startActivity(displayIntent)
+        }
+
+        val displayMedia = findViewById<Button>(R.id.media)
+        displayMedia.setOnClickListener {
+            val displayIntent = Intent(this, Media::class.java)
+            startActivity(displayIntent)
+        }
+
+        val displaySettings = findViewById<Button>(R.id.settings)
+        displaySettings.setOnClickListener {
+            val displayIntent = Intent(this, Settings::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
